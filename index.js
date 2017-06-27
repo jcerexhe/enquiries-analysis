710a83c70100ce13f17522e6ca5cb5a5d9d57f23
Jamie Cerexhe <jcerexhe@gmail.com>
2017-06-27 21:30:06 +1000

    first working draft

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const language = Language({
 });
 
 const textDoc = require('./textDoc');
+// const textDoc = require('./sampleText');
 
 // The text to analyze
 // Make sure to add it as its in gitignore
@@ -23,14 +24,47 @@ document.detectEntities()
   .then((results) => {
     const entities = results[1].entities;
 
+    let items = [];
+
     console.log('Entities:');
     entities.forEach((entity) => {
-      console.log(entity.name);
-      // console.log(` - Type: ${entity.type}, Salience: ${entity.salience}`);
+      function findEntity(obj) {
+        return obj.name === entity.name;
+      }
+
+      // return entity if it exists
+      let foundEntity = items.find(findEntity);
+      // if above line is undefined then below with be false
+      if (!!foundEntity) {
+        foundEntity.count = foundEntity.count + 1;
+        // console.log('return:', items.find(findEntity)); 
+      } else {
+        let newEntity = new Object();
+        newEntity.name = entity.name;
+        newEntity.count = 0;
+        items.push(newEntity);
+      }
+      
+      // console.log(`${entity.name} - Type: ${entity.type}, Salience: ${entity.salience}`);
       // if (entity.metadata && entity.metadata.wikipedia_url) {
       //   console.log(` - Wikipedia URL: ${entity.metadata.wikipedia_url}$`);
       // }
     });
+
+    items.sort(function(a, b) {
+      return b.count - a.count;
+    })
+    // console.log(items);
+
+    function formatCSV(entityArray) {
+      let csv = [];
+      // csv format with just comma separated or with new line? How to easily import into excel?
+      entityArray.forEach(entity => csv.push(`${entity.count} - ${entity.name}`));
+      csv.join(', ');
+      return csv;
+    }
+
+    console.log(formatCSV(items))
   })
   .catch((err) => {
     console.error('ERROR:', err);
